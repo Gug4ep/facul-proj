39624cc28763f334ba15ebb93f44ac453f354127
Guga <gustavo.eger@clinicorp.com>
2022-07-01 19:10:10 -0300

    initialize-config-firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAZXE-VOrHK8CM4b4PPFVWX4Agoe6zS7Q",
+  authDomain: "gustavoep-treinamento.firebaseapp.com",
+  projectId: "gustavoep-treinamento",
+  storageBucket: "gustavoep-treinamento.appspot.com",
+  messagingSenderId: "96482144265",
+  appId: "1:96482144265:web:2f3a268b6e218ecd5fc8d8",
+  measurementId: "G-75CYVJCH4S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
